b2689f22da68f5b77acaa180cd9839c284543bd0
Viljami <viljami.ketola@haltu.fi>
2017-05-08 17:04:42 +0300

    Added the rest of the base structure

--- src/palautebot/palautebot/settings.py
+++ src/palautebot/palautebot/settings.py
@@ -3,7 +3,7 @@
 from django.conf import settings
 
 
-TWITTER_CLIENT_KEY = getattr(settings, 'TWITTER_CLIENT_KEY', None)
-TWITTER_CLIENT_SECRET = getattr(settings, 'TWITTER_CLIENT_SECRET', None)
+TWITTER_CLIENT_KEY = getattr(settings, 'TWITTER_CONSUMER_KEY', None)
+TWITTER_CLIENT_SECRET = getattr(settings, 'TWITTER_CONSUMER_SECRET', None)
 TWITTER_ACCESS_TOKEN = getattr(settings, 'TWITTER_ACCESS_TOKEN', None)
 TWITTER_ACCESS_TOKEN_SECRET = getattr(settings, 'TWITTER_ACCESS_TOKEN_SECRET', None)
