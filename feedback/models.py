0e912c93285126982847b0fef3898870c026bbae
Tuomas Haapala <tuomas.haapala@anders.fi>
2018-02-14 00:42:43 +0200

    New project base structure
    Changed project base structure to be more consistent with Helsinki
    projects style and general good habits.
    
    * Renamed Django apps project -> palautebot and  palautebot -> feedback
    * Added pip-tools based requirements
    * Changed settings file to match other HKI projects
    * Added flake8 and isort and ran them
    * Removed Heroku related and other non needed stuff
    * Added Travis conf
    * Slight tuning for the only model
    * Other small enhancements here and there
    
    The commit contains a new initial migration so possible existing db
    needs to be nuked.
    
    Refs #9

---
+++ feedback/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.translation import pgettext_lazy
+
+
+class Feedback(models.Model):
+    SOURCE_TWITTER = 'twitter'
+
+    SOURCE_CHOICES = (
+        (SOURCE_TWITTER, 'Twitter'),
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    source = models.CharField(max_length=2048, choices=SOURCE_CHOICES)
+    source_id = models.CharField(max_length=2048)
+    source_created_at = models.DateTimeField(null=True, blank=True)
+    ticket_id = models.CharField(max_length=2048, blank=True, db_index=True)
+
+    class Meta:
+        verbose_name = pgettext_lazy('singular', 'feedback')
+        verbose_name_plural = pgettext_lazy('plural', 'feedback')
+        unique_together = ('source', 'source_id')
+
+    def __str__(self):
+        return '{} {}'.format(self.source, self.source_id)
