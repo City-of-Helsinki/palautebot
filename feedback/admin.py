66958438618ddd1577dc280eb2ea10bc135c81a4
Tuomas Haapala <tuomas.haapala@anders.fi>
2018-02-21 08:50:20 +0200

    New data model
    Added models for tweets and (twitter) direct messages, because every
    tweet that needs to be tracked isn't necessary tied to a feedback item
    as the old data structure assumed. And we also need direct messages
    that behave a bit differently than tweets.
    
    Includes a new initial migration.
    
    Refs #3

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Feedback
+from .models import DirectMessage, Feedback, Tweet
 
+admin.site.register(DirectMessage)
 admin.site.register(Feedback)
+admin.site.register(Tweet)
